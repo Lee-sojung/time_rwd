11e708d1bbd71d68af2cdea5b60ad02876120aab
Lee-sojung <thwjd3698@gmail.com>
2022-01-10 11:30:55 +0900

    시간에 따라 최상위 부모요소 클래스명 변경

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 ﻿const time = document.querySelectorAll('.screen span');
+const main = document.querySelector('main');
+const menus = document.querySelector('nav span');
 
 setInterval(()=>{
     let now = new Date();
     let hr = now.getHours();
     let min = now.getMinutes();
     let sec = now.getSeconds();
+
+    if(hr >= 5 && hr < 11){
+        //혹시라도 있을지모르는 클래스명 싹다 비우기
+        main.className = "";
+        main.classList.add('morning');
+    }
+    if(hr >= 11 && hr<15){
+        main.className = "";
+        main.classList.add("afternoon");
+    }
+    if(hr >=16 && hr<19){
+        main.className = "";
+        main.classList.add('evening');
+    }
+    if(hr >= 19 || hr<5){
+        main.className ="";
+        main.classList.daa('night');
+    }
 
     if(hr < 10) hr ='0'+hr;
     if(min < 10) min = '0'+min;

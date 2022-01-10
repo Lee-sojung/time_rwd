7d0082e79b7b921f38298d9b26876cc3afae5591
Lee-sojung <thwjd3698@gmail.com>
2022-01-10 11:48:00 +0900

    시간 보정함수 추가

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-﻿const time = document.querySelectorAll('.screen span');
-const main = document.querySelector('main');
+﻿const main = document.querySelector('main');
 const menus = document.querySelector('nav span');
+const numbers = document.querySelectorAll('.screen span');
 
 setInterval(()=>{
-    let now = new Date();
-    let hr = now.getHours();
-    let min = now.getMinutes();
-    let sec = now.getSeconds();
+
+    const times = setTime();
+    times.forEach((time, index) =>getTime(time, index));
+
 
     if(hr >= 5 && hr < 11){
         //혹시라도 있을지모르는 클래스명 싹다 비우기
@@ -26,16 +26,29 @@ setInterval(()=>{
         main.classList.daa('night');
     }
 
-    if(hr < 10) hr ='0'+hr;
-    if(min < 10) min = '0'+min;
-    if(sec < 10) sec = '0'+sec;
 
-    time[0].innerText = hr;
-    time[1].innerText = min;
-    time[2].innerText = sec;
+    // if(hr < 10) hr ='0'+hr;
+    // if(min < 10) min = '0'+min;
+    // if(sec < 10) sec = '0'+sec;
+
+    // time[0].innerText = hr;
+    // time[1].innerText = min;
+    // time[2].innerText = sec;
 },1000)
 
+function setTime(){
+    let now = new Date();
+    let hr = now.getHours();
+    let min = now.getMinutes();
+    let sec = now.getSeconds();
 
+    return[hr, min, sec];
+}
+
+function getTime(num, index){
+    if(num<10) num = '0'+num;
+    numbers[index].innerText = num;
+}
 
 
 
